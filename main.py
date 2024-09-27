72fe4e176800d7d9447568e45effdf6f92db4fe1
Maarten Warnez <mrtnwrnz@gmail.com>
2024-09-27 15:42:50 +0200

    2nd attempt

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-main 
+import table.py
+import openspace.py
+import file_utils.py
+import random
+
+names_all_a = ["Adheeba","Anastasiia", "Basma", "Dhrisya", "Ihor", "Izabela", 
+                    "Kelli", "Kevin", "Levin", "Maarten", "Majid", "Minh Duc", "Moustafa", 
+                    "Muntadher", "Nicolaas", "Petra", "Rasmita", "Rik", "Soha", "Tom", 
+                    "Urson", "Veena", "Wouter", "Yeliz"]
+names_all = random.shuffle(names_all_a)
+
+# Create 24 empty seats and put them in a list
+seats_all = [Seat() for i in range(23)]
+
+# Combine both lists as key-value pairs in a dictionary
+seats_names = dict(zip(seats_all, names_all))
+
+
+
+
+
+
+
--- utils/table.py
+++ utils/table.py
@@ -1,17 +1,35 @@
-"""
-class Seat(free, occupant)
+class Seat:
+    def __init__(self, free, occupant):
+        self.free = True
+        self.occupant = ""
+        
+    def set_occupant(self, name):
+        self.occupant = name
+        self.free = False
 
-    def set_occupant(name)
+    def remove_occupant(self):
+        self.free = True
+        self.occupant = ""
 
-    def remove occupant()
+class Table():
+    def __str__(self):
+        return "test"
 
-class Table()
-    self.capacity = int
-    self.seats = list(Seat obj)
+    def __init__(self):
+        self.capacity = 4
+        self.seats = []
+        tables_all.append(self)
 
-    def has_free_spot()
+    def has_free_spot(self):
+        return self.capacity > len(self.seats)
 
-    def assign_seat(name)
+    def assign_seat(self, seat):
+        self.seats.append(seat)
 
-    def left_capacity():
-        return int
+    def left_capacity(self):
+        return self.capacity - len(self.seats)
+
+maarten = Seat(True, "Maarten")
+tafel_1 = Table()
+tafel_1.assign_seat(maarten)
+print(maarten)
